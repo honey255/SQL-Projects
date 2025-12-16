7e5f576a6c07458429cf1ec270e259bdb2d21c94
Priyanshu Siwach <83805676+honey255@users.noreply.github.com>
2025-12-16 11:36:35 +0530

    Delete Pizza Sales Project/Queryes directory

--- Pizza Sales Project/Queryes/Q2.sql
+++
@@ -1,6 +0,0 @@
--- 2. Calculate the total revenue generated from pizza sales.
-
-SELECT ROUND(SUM(od.quantity * pz.price),2) AS Total_sales
-FROM orders_details AS od
-JOIN pizzas AS pz
-ON od.pizza_id = pz.pizza_id;
